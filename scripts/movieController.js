5d592f288ab9b55cdd3ed706a0a5fc5f0b2327f9
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2017-11-29 13:26:46 -0600

    added basic rating star functionality

--- scripts/movieController.js
+++ scripts/movieController.js
@@ -7,6 +7,7 @@
 const firebase = require("firebase")
 const movieFactory = require("./movieFactory")
 const searchStoredMovies = require("./searchStored")
+const reviewStars = require("./stars")
 
 // Create object to contain a function. Will be updated
 const movieController = Object.create(null, {
@@ -59,6 +60,10 @@ const movieController = Object.create(null, {
                     // jq call the id, print html into that div
                     $("#search_db-results").html(resultEl)
 
+
+
+                    //add the second search bar to the dom...
+
                     //add listener to add to watch list button
                     $(".addToWatchlist").on("click", e => {
                         movieController.storeMovie(e.target.id)
@@ -86,42 +91,84 @@ const movieController = Object.create(null, {
         value: function () {
             movieFactory.all()
                 .then(function (response) {
-                    debugger
                     let resultEl = ""
                     console.log(response)
                     response.filter(
                         movieObj => firebase.auth().currentUser.uid === movieObj.uid && movieObj.watched === false).forEach(
-                            movie => {
-                                    if (movie.movie.poster_path === null) {
-                                    resultEl += `
+                        movie => {
+                            if (movie.movie.poster_path === null) {
+                                resultEl += `
                                     <div class="card-block" style="width: 20rem;">
                                         <img class="card-img-top" src="http://www.51allout.co.uk/wp-content/uploads/2012/02/Image-not-found-300x300.gif" alt="Card image cap">
                                         <div class="card-block_inner">
                                             <h4 class="card-title">${movie.movie.title}</h4>
                                             <p class="card-text">Release Date${movie.movie.release_date}</p>
                                             <p class="card-text">Cast: ${movie.movie.credits.cast[0].name}, ${movie.movie.credits.cast[2].name}, ${movie.movie.credits.cast[3].name}</p>
+                                            <section class='rating-widget'>
+
+                                            <div class='rating-stars text-center'>
+                                              <ul id='stars'>
+                                                <li class='star' title='Poor' data-value='1'>
+                                                  <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='Fair' data-value='2'>
+                                                  <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='Good' data-value='3'>
+                                                  <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='Excellent' data-value='4'>
+                                                  <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='WOW!!!' data-value='5'>
+                                                  <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                              </ul>
                                             </div>
+                                        </div>
                                     </div>
                                     `
-                                } else {
-                                    resultEl += `
+                            } else {
+                                resultEl += `
                                     <div class="card-block" style="width: 20rem;">
                                         <img class="card-img-top" src="https://image.tmdb.org/t/p/w185//${movie.movie.poster_path}" alt="Card image cap">
                                         <div class="card-block_inner">
                                             <h4 class="card-title">${movie.movie.title}</h4>
                                             <p class="card-text">Release Date${movie.movie.release_date}</p>
                                             <p class="card-text">Cast: ${movie.movie.credits.cast[0].name}, ${movie.movie.credits.cast[2].name}, ${movie.movie.credits.cast[3].name}</p>
+                                            <section class='rating-widget'>
+
+                                            <div class='rating-stars text-center'>
+                                                <ul id='stars'>
+                                                <li class='star' title='Poor' data-value='1'>
+                                                    <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='Fair' data-value='2'>
+                                                    <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='Good' data-value='3'>
+                                                    <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='Excellent' data-value='4'>
+                                                    <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                <li class='star' title='WOW!!!' data-value='5'>
+                                                    <i class='fa fa-star fa-fw'></i>
+                                                </li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                     `
-                                }
                             }
+                        }
                         )
                     $('#search_db-results').html(resultEl)
-
+                    //add star functionality
+                    reviewStars()
                 })
 
-                searchStoredMovies.init()
+            searchStoredMovies.init()
 
         }
     }
--- scripts/searchStored.js
+++ scripts/searchStored.js
@@ -1,3 +1,5 @@
+//Author: Max Wolf
+
 const movieFactory = require("./movieFactory")
 const firebase = require("firebase")
 
@@ -11,35 +13,38 @@ const searchStoredMovies = Object.create(null, {
             let searchQuery = document.getElementById("search_input-stored").value.toLowerCase()
             let resultEl = ""
 
+            //if there are 3 characters in the search array, populate the dom with the filtered movie array
             if (searchQuery.length >= 3) {
+                //define filteredMovies as the movies that include the searchQuery in the title
                 let filteredMovies = movieFactory.cache.filter(movies => {
                     return movies.movie.title.toLowerCase().includes(searchQuery);
                 })
 
-                //if there are 3 characters in the search array, populate the dom with the filtered movie array
-                filteredMovies.forEach(
-                    movie => {
-                        resultEl += `
-                        <div class="card-block" style="width: 20rem;">
-                            <img class="card-img-top" src="https://image.tmdb.org/t/p/w185//${movie.movie.poster_path}" alt="Card image cap">
-                            <div class="card-block_inner">
-                                <h4 class="card-title">${movie.movie.title}</h4>
-                                <p class="card-text">Release Date${movie.movie.release_date}</p>
-                                <p class="card-text">Cast: ${movie.movie.credits.cast[0].name}, ${movie.movie.credits.cast[2].name}, ${movie.movie.credits.cast[3].name}</p>
-                                </div>
-                        </div>`
-
+                //filter only movies stored for the current user
+                filteredMovies.filter(movieObj =>
+                    firebase.auth().currentUser.uid === movieObj.uid && movieObj.watched === false)
+                    //loop through each movie and write the title, poster, releasedate, cast, etc. to resultEl
+                    .forEach(
+                        movie => {
+                            resultEl += `
+                            <div class="card-block" style="width: 20rem;">
+                                <img class="card-img-top" src="https://image.tmdb.org/t/p/w185//${movie.movie.poster_path}" alt="Card image cap">
+                                <div class="card-block_inner">
+                                    <h4 class="card-title">${movie.movie.title}</h4>
+                                    <p class="card-text">Release Date${movie.movie.release_date}</p>
+                                    <p class="card-text">Cast: ${movie.movie.credits.cast[0].name}, ${movie.movie.credits.cast[2].name}, ${movie.movie.credits.cast[3].name}</p>
+                                    </div>
+                            </div>`
                         })
-
+                //write the resultEl to the DOM
                 $('#search_db-results').html(resultEl)
 
-                //if there are no results
+                //if there are no search results then write 'No Results' to the DOM
                 if (filteredMovies.length === 0) {
-                    document.getElementById("search_db-results").innerHTML = "<div id='noResults'>No Results</div>";
+                    document.getElementById("search_db-results").innerHTML = "<div id='noResults'>Sorry, there are no movies that match your search</div>";
                 }
 
-            //if there are less than 3 characters in the input text box, then display the storedmovieArticles
-            //THIS NEEDS TO REDISPLAY THE WHOLE CACHE WHEN LESS THAN 3 CHARS IN SEARCH BOX
+            //if there are less than 3 characters in the input text box, then display the cache of storedmovies
             } else if (searchQuery.length < 3) {
                 movieFactory.cache.filter(
                     movieObj => firebase.auth().currentUser.uid === movieObj.uid && movieObj.watched === false).forEach(
