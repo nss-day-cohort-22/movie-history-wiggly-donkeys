a70940c89ae1a79c9787a3f1eb1050cd5c5b2d94
Paul Ellis <tynesellis@gmail.com>
2017-11-29 13:26:03 -0600

    modular dom string

--- scripts/movieController.js
+++ scripts/movieController.js
@@ -7,6 +7,7 @@
 const firebase = require("firebase")
 const movieFactory = require("./movieFactory")
 const searchStoredMovies = require("./searchStored")
+const cardsHTML = require("./cardsHTML")
 
 // Create object to contain a function. Will be updated
 const movieController = Object.create(null, {
@@ -87,37 +88,9 @@ const movieController = Object.create(null, {
             movieFactory.all()
                 .then(function (response) {
                     let resultEl = ""
-                    console.log(response)
                     response.filter(
                         movieObj => firebase.auth().currentUser.uid === movieObj.uid && movieObj.watched === false).forEach(
-                            movie => {
-                                    if (movie.movie.poster_path === null) {
-                                    resultEl += `
-                                    <div class="card-block" style="width: 20rem;">
-                                        <button class="deleteMovie">X</button>                                                                                                                            
-                                        <img class="card-img-top" src="http://www.51allout.co.uk/wp-content/uploads/2012/02/Image-not-found-300x300.gif" alt="Card image cap">
-                                        <div class="card-block_inner">
-                                            <h4 class="card-title">${movie.movie.title}</h4>
-                                            <p class="card-text">Release Date${movie.movie.release_date}</p>
-                                            <p class="card-text">Cast: ${movie.movie.credits.cast[0].name}, ${movie.movie.credits.cast[2].name}, ${movie.movie.credits.cast[3].name}</p>
-                                            <button class="deleteMovie">X</button>                                                                                     
-                                            </div>
-                                    </div>
-                                    `
-                                } else {
-                                    resultEl += `
-                                    <div class="card-block" style="width: 20rem;">
-                                        <button class="deleteMovie">X</button>                                                                                                                            
-                                        <img class="card-img-top" src="https://image.tmdb.org/t/p/w185//${movie.movie.poster_path}" alt="Card image cap">
-                                        <div class="card-block_inner">
-                                            <h4 class="card-title">${movie.movie.title}</h4>
-                                            <p class="card-text">Release Date${movie.movie.release_date}</p>
-                                            <p class="card-text">Cast: ${movie.movie.credits.cast[0].name}, ${movie.movie.credits.cast[2].name}, ${movie.movie.credits.cast[3].name}</p>
-                                        </div>
-                                    </div>
-                                    `
-                                }
-                            }
+                            movie => resultEl += cardsHTML(movie)
                         )
                     $('#search_db-results').html(resultEl)
 
